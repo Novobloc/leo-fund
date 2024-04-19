7145426732400f5b5a43e0bd06f2e838725c02d0
Satya Sandeep <satyasandeep786@gmail.com>
2024-04-19 22:20:04 +0530

    feat: Integrate Tailwind css

---
+++ src/Routing.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./layout/Header";
+
+const Routing: React.FC = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default Routing;
